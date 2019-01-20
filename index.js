c0c1b472585fe9b28b4e027ff765577fb8af3264
Cezary <cezary.tworzewski@gmail.com>
2019-01-20 22:56:59 +0100

    change design

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-const display = document.querySelector(".display");
-const input = document.querySelector(".button");
+/* const display = document.querySelector(".display");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const plus = document.querySelector(".plus");
+const equal = document.querySelector(".equal");
 
-console.log(input);
+function jeden() {
+  display.textContent = btn1.value;
+}
 
-input.addEventListener("click", function() {
-  display.textContent = input.value;
-  console.log("teścik");
+function dwa() {
+  display.textContent = btn2.value;
+}
+
+function add() {
+  display.textContent = plus.value;
+}
+function result() {
+  return btn1.value + btn2.value;
+}
+
+btn1.addEventListener("click", jeden);
+btn2.addEventListener("click", dwa);
+plus.addEventListener("click", add);
+equal.addEventListener("click", result);
+
+// console.log(button);
+/* 
+for (let i = 0; i < button.length; i++) {
+  console.log(button[i]);
+} */
+
+/* input.forEach(function(btn) {
+  console.log(btn);
 });
+
+function test() {
+  console.log("działa");
+} */
+
+// input.addEventListener("click", test);

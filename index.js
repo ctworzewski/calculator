9557c22bb987064d6c361dd1a1a300224594b7f1
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2019-01-24 19:21:32 +0100

    add to display number, can write a few number in output (screenRef)

--- index.js
+++ index.js
@@ -5,36 +5,38 @@ const clearRef = document.querySelector(".clear");
 
 // screenRef.textContent = "dd";
 
-const num = numberRef.forEach(function (number) {
-  number.addEventListener("click", function () {
+const num = numberRef.forEach(function(number) {
+  number.addEventListener("click", function() {
     // parseInt(number.value);
     // parseInt((screenRef.textContent = number.value));
     // screenRef.textContent = number.value;
     // console.log(number.value);
+
     let temp = number.value;
 
     // console.log(temp);
     temp = screenRef.textContent + number.value;
-    screenRef.textContent = temp + number.value;
+    // screenRef.textContent = temp + number.value;
+    screenRef.textContent = temp;
     // console.log(temp);
     // temp.textContent += number.value;
     // console.log('to: ', temp.textContent += number)
   });
 });
 
-const oper = operatorRef.forEach(function (operator) {
-  operator.addEventListener("click", function () {
+const oper = operatorRef.forEach(function(operator) {
+  operator.addEventListener("click", function() {
     screenRef.textContent = operator.value;
     // console.log(typeof operator);
   });
 });
 console.log(oper);
 
-function add() { }
-function sub() { }
-function multi() { }
-function div() { }
+function add() {}
+function sub() {}
+function multi() {}
+function div() {}
 
-const clear = clearRef.addEventListener("click", function () {
+const clear = clearRef.addEventListener("click", function() {
   screenRef.value = "";
 });
